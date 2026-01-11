443e61ae4d99448af55a44493e89d8796ed97ad6
D-codeTillDie <125109506+Davidthecoderme@users.noreply.github.com>
2026-01-10 20:19:55 -0500

    update

--- 04_langgraph_task_manager_agent_starter/backend/src/utils/env.ts
+++ 04_langgraph_task_manager_agent_starter/backend/src/utils/env.ts
@@ -1,25 +1,62 @@
-
 import dotenv from 'dotenv';
 import {z} from 'zod'
 
 dotenv.config();
 
 const EnvSchema = z.object({
-    GROQ_API_KEY: z.string().min(1 , "GROQ_API_KEY is required"),
+    GROQ_API_KEY: z.string().min(1 , 'GROQ_API_KEY is required'),
     GROQ_MODEL: z.string().default("llama-3.1-8b-instant"),
-    PORT: z.string().default("5000"),
+    PORT: Number(z.string().default("5000"))
 })
 
-const parsed = EnvSchema.safeParse(process.env);
+const parsed  = EnvSchema.safeParse(process.env)
 
-if(parsed.success === false){
-    throw new Error("Error occured while parsing env variables " + JSON.stringify(parsed.error.format()))
+if(parsed.success === false) {
+    throw new Error ("Error occured while parsing env variables" + JSON.stringify(parsed.error.format()))
 }
 
-const raw = parsed.data;
+const raw = parsed.data
 
 export const env = Object.freeze({
     GROQ_API_KEY: raw.GROQ_API_KEY,
     GROQ_MODEL: raw.GROQ_MODEL,
     PORT: Number(raw.PORT),
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// dotenv.config();
+
+// const EnvSchema = z.object({
+//     GROQ_API_KEY: z.string().min(1 , "GROQ_API_KEY is required"),
+//     GROQ_MODEL: z.string().default("llama-3.1-8b-instant"),
+//     PORT: z.string().default("5000"),
+// })
+
+// const parsed = EnvSchema.safeParse(process.env);
+
+// if(parsed.success === false){
+//     throw new Error("Error occured while parsing env variables " + JSON.stringify(parsed.error.format()))
+// }
+
+// const raw = parsed.data;
+
+// export const env = Object.freeze({
+//     GROQ_API_KEY: raw.GROQ_API_KEY,
+//     GROQ_MODEL: raw.GROQ_MODEL,
+//     PORT: Number(raw.PORT),
+// })
